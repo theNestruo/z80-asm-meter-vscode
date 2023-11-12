a5ad750fc4f65e7354a8d32f3fed7b374e47b48a
Néstor Sancho <theNestruo@gmail.com>
2023-11-12 09:17:36 +0100

    CodeFactor issue

--- src/model/SourceCode.ts
+++ src/model/SourceCode.ts
@@ -19,7 +19,7 @@ export function extractSourceCode(rawLine: string,
 
         // For every part
         let currentPart = "";
-        let quoted = null;
+        let quoted = undefined;
         let whitespace = -1;
         for (; i < n; i++) {
             const c = s.charAt(i);
@@ -28,18 +28,14 @@ export function extractSourceCode(rawLine: string,
             if (quoted) {
                 currentPart += c;
                 if (c === quoted) {
-                    quoted = null;
+                    quoted = undefined;
                 }
                 continue;
             }
 
             // Comment?
-            const isCommentStart = (c === ";")
-                || (c === "/") && (i + 1 < n) && (s.charAt(i + 1) === "/")
-                ? c
-                : undefined;
-            if (isCommentStart) {
-                const lineComment = s.substring(isCommentStart === ";" ? i + 1 : i + 2).trim();
+            const lineComment = isLineCommentStart(c, s, i, n);
+            if (lineComment !== undefined) {
                 fragments.push(new SourceCode(currentPart, repetitions, lineComment));
                 return fragments;
             }
@@ -62,12 +58,7 @@ export function extractSourceCode(rawLine: string,
             whitespace = 0;
 
             // Quote?
-            if ((c === "\"")
-                // Prevents considering "'" as a quote
-                // when parsing the instruction "ex af,af'"
-                || ((c === "'") && (!/^ex\s+af\s*,\s*af$/i.test(currentPart)))) {
-                quoted = c;
-            }
+            quoted = isQuote(c, currentPart);
 
             currentPart += c.toUpperCase();
         }
@@ -100,6 +91,23 @@ function removeSurroundingLabelAndRepetitions(
         }
     }
     return [repetitions, s];
+}
+
+function isLineCommentStart(
+    c: string, line: string, i: number, n: number): string | undefined {
+
+    return (c === ";") ? line.substring(i + 1).trim()
+        : (c === "/") && (i + 1 < n) && (line.charAt(i + 1) === "/") ? line.substring(i + 2).trim()
+        : undefined;
+}
+
+function isQuote(c: string, currentPart: string): string | undefined {
+
+    return (c === "\"") ? c
+        // Prevents considering "'" as a quote
+        // when parsing the instruction "ex af,af'"
+        : ((c === "'") && (!/^ex\s+af\s*,\s*af$/i.test(currentPart))) ? c
+        : undefined;
 }
 
 /**
