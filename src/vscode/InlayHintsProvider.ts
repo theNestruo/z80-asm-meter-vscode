e0fb78aa0d9ea0757078950936b7d6d47147e044
Néstor Sancho <theNestruo@gmail.com>
2024-12-19 13:06:33 +0100

    Fixes double space on text copied to clipboard

--- src/vscode/InlayHintsProvider.ts
+++ src/vscode/InlayHintsProvider.ts
@@ -412,7 +412,7 @@ class InlayHintCandidate {
 		const timingSuffix = printableTimingSuffix();
 
 		// Computes the InlayHint label
-		const label = `${timing}${timingSuffix}`;
+		const label = `${timing} ${timingSuffix}`;
 
 		return new InlayHint(this.position, label, this.paddingLeft, this.paddingRight,
 			this.range, totalTimings, totalTiming, timing, timingSuffix, this.sourceCode[0]);
